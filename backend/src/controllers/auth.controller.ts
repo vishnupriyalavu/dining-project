61c155b5eb29976ffd0eb1f007dffc15cd4b4a0c
L Vishnu Priya <l.vishnupriya@bounteous.com>
2026-03-10 14:27:59 +0530

    feat(auth): implement register and login APIs with JWT

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from "express"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import {prisma} from "../config/prisma"
+
+export const registerUser = async (req: Request, res: Response) => {
+  const { name, email, password } = req.body
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword
+      }
+    })
+
+    res.status(201).json(user)
+
+  } catch (error) {
+    res.status(500).json({ message: "Registration failed" })
+  }
+}
+
+export const loginUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email }
+    })
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" })
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" })
+    }
+
+    const token = jwt.sign(
+      { userId: user.id },
+      process.env.JWT_SECRET || "secret",
+      { expiresIn: "1d" }
+    )
+
+    res.json({ token })
+
+  } catch (error) {
+    res.status(500).json({ message: "Login failed" })
+  }
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,18 @@
 import express, { Request, Response } from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import authRoutes from "./routes/auth.routes"
 
 dotenv.config()
 
 const app = express()
 
+// Middleware
 app.use(cors())
 app.use(express.json())
+
+// Routes
+app.use("/auth", authRoutes)
 
 const PORT = process.env.PORT || 5000
 
