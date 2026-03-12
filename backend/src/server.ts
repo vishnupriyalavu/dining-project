af0e09b22fda08cce1928253cc8f35ec39957857
L Vishnu Priya <l.vishnupriya@bounteous.com>
2026-03-12 12:55:33 +0530

    changed the service

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,7 +12,9 @@ dotenv.config()
 const app = express()
 
 // Middleware
-app.use(cors())
+app.use(cors({origin: "http://localhost:5173",
+  credentials:true}
+))
 app.use(express.json())
 
 // Routes
